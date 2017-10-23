70aa194d1151eac9f70df9a0ac9fae3134219c34
Jean-Baptiste <mooftarkin@hotmail.fr>
2017-10-23 13:58:12 +0200

    petri net half arena

--- Project Code/The Arena/arena_pn.m
+++ Project Code/The Arena/arena_pn.m
@@ -5,10 +5,11 @@ global_info.STOP_AT = 15;
 pns = pnstruct('arena_pn_pdf');
 
 dynamic.m0 = {'py1', 1}; % # of tokens initially
-dynamic.ft = {'ty1', 3, 'ty2', 7, 'tTower', 3}; % seconds each transition takes
+dynamic.ft = {'allothers',1};
 pni = initialdynamics(pns, dynamic);
 
 sim = gpensim(pni);
 
-plotp(sim, {'py1', 'pyRamp', 'py2'});
+plotp(sim, {'py1','pyRamp','py2', 'pyBasket','pmc1', 'pmc2', 'pmc3', 'pyTower', 'pyc1', 'pyc2', 'pc2Tower','pslot2', 'pslot4', 'pslot5', 'psba2', 'psba4', 'pbba2'})
+
 prnss(sim);
