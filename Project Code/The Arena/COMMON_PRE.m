5b5c7a3727a67ba394089c94686e1944157b66d3
Jean-Baptiste <mooftarkin@hotmail.fr>
2017-10-30 13:10:24 +0100

    transition slot2 in common_pre

--- Project Code/The Arena/COMMON_PRE.m
+++ Project Code/The Arena/COMMON_PRE.m
@@ -1,7 +1,29 @@
 function [fire, transition] = COMMON_PRE(transition)
 
-transition.new_color='wheels';
-transition.override = 1;
-fire=1;
+
+    if (strcmp(transition.name,"tslot2_1")||strcmp(transition.name,"tslot2_1")||strcmp(transition.name,"tslot2_1"))
+        tokenRrobot = tokenAny('pslot2', 1); 
+        colors = get_color('pslot2',tokenRobot);
+        
+        tokCylinder = tokenAnyColor('pslot2',1,{'cylinder'})
+        transition.selected_tokens = tokCylinder;
+
+        transition.override=1;
+        for i=1:length(colors)
+            cylinder=0;
+            if(not(strcmp(colors{i},'cylinder'))||cylinder==1)
+                transition.new_color{1,length(transition)+1}=colors{i};
+            else
+                cylinder=1;
+            end
+        end
+        transition.new_color{1,length(transition)+1}='openTong'
+        fire = (tokCylinder)
+    
+    else
+        transition.new_color='wheels';
+        transition.override = 1;
+        fire=1;
+    end
 end
 
