8764ffa7d06c803fa6c8f5de08c8081635e77b96
Jean-Baptiste <mooftarkin@hotmail.fr>
2017-10-30 15:22:11 +0100

    moving robot with cylinder (need correction of color)

--- Project Code/The Arena/COMMON_PRE.m
+++ Project Code/The Arena/COMMON_PRE.m
@@ -1,29 +1,39 @@
 function [fire, transition] = COMMON_PRE(transition)
 
+    global global_info;
 
-    if (strcmp(transition.name,"tslot2_1")||strcmp(transition.name,"tslot2_1")||strcmp(transition.name,"tslot2_1"))
-        tokenRrobot = tokenAny('pslot2', 1); 
+    if (strcmp(transition.name,"tslot2_1")||strcmp(transition.name,"tslot2_2")||strcmp(transition.name,"tslot2_3"))
+        tokenRobot = tokenAny('pslot2', 1); 
         colors = get_color('pslot2',tokenRobot);
         
-        tokCylinder = tokenAnyColor('pslot2',1,{'cylinder'})
-        transition.selected_tokens = tokCylinder;
+    %    tokCylinder = tokenAnyColor('pslot2',1,{'cylinder'});
+    %    global_info.token= tokCylinder;
+    %    transition.selected_tokens = tokCylinder;
 
         transition.override=1;
+        transition.new_color={};
         for i=1:length(colors)
             cylinder=0;
             if(not(strcmp(colors{i},'cylinder'))||cylinder==1)
-                transition.new_color{1,length(transition)+1}=colors{i};
+                transition.new_color{1,length(transition.new_color)+1}=colors{i};
             else
                 cylinder=1;
             end
         end
-        transition.new_color{1,length(transition)+1}='openTong'
-        fire = (tokCylinder)
+        transition.new_color{1,length(transition.new_color)+1}='openTong';
+        fire =1;
     
     else
-        transition.new_color='wheels';
-        transition.override = 1;
-        fire=1;
+        if(strcmp(transition.name,'tmCylinder1')||strcmp(transition.name,'tmCylinder2')||strcmp(transition.name,'tmCylinder3')||strcmp(transition.name,'tyCylinder1')||strcmp(transition.name,'tyCylinder2')) 
+   %         place=strcat('p',transition.name(2),'c',transition.name(11));
+    %        tokTake=tokenWOAnyColor(place,'2','cylinder');
+            transition.new_color='cylinder';
+            fire=1;
+        else
+            transition.new_color='wheels';
+          %  transition.override = 1;
+            fire=1;
+        end
     end
 end
 
--- Project Code/The Arena/arena_pn.m
+++ Project Code/The Arena/arena_pn.m
@@ -1,16 +1,28 @@
 close all; clear all; clc;
 global global_info;
 global_info.STOP_AT = 15;
+global_info.token='';
 
 pns = pnstruct('arena_pn_pdf');
 
-dynamic.m0 = {'py1', 1}; % # of tokens initially
+dynamic.m0 = {'py1', 1,'pmCylinder1',1,'pmCylinder2',1,'pmCylinder3',1,'pyCylinder1',1,'pyCylinder2',1}; % # of tokens initially
 dynamic.ft = {'allothers',1};
+dynamic.ip = {'tpy1_pyRamp',1,'tpyRamp_py2',1,'tpy2_pyTower',1,'tmCylinder1',2,'tmCylinder2',2,'tmCylinder3',2,'tyCylinder1',2,'tyCylinder2',2,'tslot2_1',2,'tslot2_2',2,'tslot2_3',2,'tpmc3_pslot2',1}
 pni = initialdynamics(pns, dynamic);
 
 sim = gpensim(pni);
 
+figure(4)
+plotp(sim,{'pmc1', 'pmc2', 'pmc3','pyc1', 'pyc2','pslot2'})
+figure(5)
 plotp(sim, {'py1','pyRamp','py2', 'pyBasket','pmc1', 'pmc2', 'pmc3', 'pyTower', 'pyc1', 'pyc2', 'pc2Tower','pslot2', 'pslot4', 'pslot5', 'psba2', 'psba4', 'pbba2'})
-plotp(sim, {'py1','pWheels','pRobot1','pCannon','pBrush'})
+global_info.token
+%plotp(sim, {'py1','pWheels','pRobot1','pCannon','pBrush'})
+figure(1)
+plotp(sim,{'pmCylinder1','pmCylinder2','pmCylinder3','pyCylinder1','pyCylinder2'})
+figure(2)
+plotp(sim,{'pslot2_1','pslot2_2','pslot2_3'})
 prnfinalcolors(sim)
+prnss(sim);
+
 %prnss(sim)
